235c7ff6413db0d85d64867cde665d732a4656d3
guerreiro777 <guerreiro777@gmail.com>
2022-11-16 20:03:25 -0300

    delecao

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from "./App";
-import registerServiceWorker from './serviceWorkerRegistration';
-
-ReactDOM.render( <App/>, document.getElementById('root') );
-
-registerServiceWorker();
